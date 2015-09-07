7fc7e5b1f93732e0ac39c25a932230fec39a98a6
Stephen Wanhella <stephen.wanhella@gmail.com>
2015-09-06 22:09:24 -0700

    added lots of styling – address, loading icon, error message

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,16 +8,31 @@ angular.module('freecodecampNightlifeAppApp')
 
     // Search yelp api for bars matching location
     $scope.search = function(place) {
+      $scope.error = null;
+      $scope.isLoading = true;
       $http.get('/api/bars/' + place).
       success(function(data) {
+        $scope.isLoading = false;
         $scope.bars = data.businesses;
+        console.dir(data.businesses);
 
         // get "going" numbers from api for each business.
         $scope.bars.forEach(function(bar, index) {
-          $http.get('/api/rsvps/' + bar.id).success(function(data) {
+          $http.get('/api/rsvps/' + bar.id).
+          success(function(data) {
             $scope.bars[index].usersGoing = data.usersGoing;
+          }).
+          error(function(err) {
+            console.log('err!', err);
           });
         });
+      }).
+      error(function(err) {
+        $scope.isLoading = false;
+        // Error getting search data
+        console.dir(JSON.parse(err.data));
+
+        $scope.error = "Error: " + JSON.parse(err.data).error.text;
       });
     };
 
@@ -25,13 +40,17 @@ angular.module('freecodecampNightlifeAppApp')
     $scope.onIsGoing = function(index) {
       var bar = $scope.bars[index];
 
-      $http.post('/api/rsvps/' + bar.id + '/' + Auth.getCurrentUser()._id).
-      success(function(data) {
-        bar.usersGoing = data.usersGoing;
-      }).
-      error(function(err) {
-        console.log('error: ' + err);
-      });
+      if (!$scope.isGoing(index)) {
+        $http.post('/api/rsvps/' + bar.id + '/' + Auth.getCurrentUser()._id).
+        success(function(data) {
+          bar.usersGoing = data.usersGoing;
+        }).
+        error(function(err) {
+          console.log('error: ' + err);
+        });
+      } else {
+        $scope.cancelRsvp(index);
+      }
     };
 
     /* return true if current user has rsvp'd to this bar */
